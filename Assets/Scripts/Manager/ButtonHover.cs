595e7866042f6b08e86071bc4f05fc15d32230aa
chittp <chiiphuong0511@gmail.com>
2026-07-08 20:32:54 +0700

    Thay doi HealthBar, tạo các button và refactor

---
+++ Assets/Scripts/Manager/ButtonHover.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHover : MonoBehaviour,
+IPointerEnterHandler,
+IPointerExitHandler
+{
+    Vector3 original;
+
+    void Start()
+    {
+        original = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = Vector3.one * 1.05f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = original;
+    }
+}
