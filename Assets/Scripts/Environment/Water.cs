2f32c0a8ab01062aeebaec91a7019cf4904e729b
chittp <chiiphuong0511@gmail.com>
2026-07-18 21:07:31 +0700

    add level 2 map 2

---
+++ Assets/Scripts/Environment/Water.cs
@@ -0,0 +1,19 @@
+//chittp
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    [SerializeField] private int damage = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        PlayerHealth health = other.GetComponent<PlayerHealth>();
+
+        if (health != null)
+        {
+            health.TakeDamage(damage);
+        }
+    }
+}
